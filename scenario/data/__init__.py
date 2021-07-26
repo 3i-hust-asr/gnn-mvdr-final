b6b22573e9f8a3a8317dfd234b9950d70923256b
thanh <thanhletienk26b@gmail.com>
2021-07-26 14:43:40 +0900

    x

--- scenario/data/__init__.py
+++ scenario/data/__init__.py
@@ -1,1 +1,2 @@
 from .gen_data import gen_data
+from .gen_rir import gen_rir
---
+++ scenario/data/gen_rir.py
@@ -0,0 +1,66 @@
+import rir_generator as rir
+import numpy as np
+
+def get_source_position(room_dimension):
+    # distance from source position and wall is 0.5 meter
+    return np.random.rand() * (np.array(room_dimension) - 1) + 0.5
+
+def get_random_direction(dim=2):
+    direction = np.random.rand(dim)
+    direction = direction / np.sqrt(np.sum(direction ** 2))
+    return direction
+
+def get_mic_positions(room_dimension, num_mic=8, d_mic=0.04):
+    origin = get_source_position(room_dimension)
+    origin = np.array(origin)
+    direction = get_random_direction() * d_mic
+    # scale ra 8 mic
+    mic_positions = [list(origin.copy())]
+    current_mic_position = list(origin.copy())
+    for mic in range(num_mic - 1):
+        current_mic_position[:2] += direction
+        mic_positions.append(list(current_mic_position.copy()))
+    return mic_positions
+
+def generate_rir_wrapper(mic_positions, source_position, room_dimension):
+    return rir.generate(c=340,                          # Sound velocity (m/s)
+                        fs=16000,                       # Sample rate (samples/s)
+                        r=mic_positions,                # Microphone position(s) [x y z] (m)
+                        s=source_position,              # Source position [x y z] (m)
+                        L=room_dimension,               # Room dimensions [x y z] (m)
+                        reverberation_time=0.5,         # Reverberation time (s)
+                        nsample=8000)                   # Number of output samples
+
+def generate_one_rir(room_dimension):
+    clean_position = get_source_position(room_dimension)
+    noise_position = get_source_position(room_dimension)
+    mic_positions  = get_mic_positions(room_dimension)
+    clean_rir      = generate_rir_wrapper(mic_positions, clean_position, room_dimension)
+    noise_rir      = generate_rir_wrapper(mic_positions, noise_position, room_dimension)
+    rir = np.concatenate([clean_rir, noise_rir], axis=1)
+    return rir    
+
+def f(idx, room_dimension):
+    pass
+
+def gen_rir(args):
+    # configuration
+    room_dimensions = {
+        'train': [(3, 3, 2), (5, 4, 6), (8, 9, 10)],
+        'dev'  : [(5, 8, 3), (4, 7, 8)],
+        'test' : [(4, 5, 3), (6, 8, 5)]
+    }
+    modes = ['train', 'dev', 'test']
+
+    num_sample = {
+        'train': 1200,
+        'dev'  : 1500,
+        'test' : 1500,
+    }
+
+    # generate rirs
+    for mode in modes:
+        idx = 0
+        for room_dimension in room_dimensions[mode]:
+            for i in range(num_sample[mode]):
+                pass
