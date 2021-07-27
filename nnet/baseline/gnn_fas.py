beab2f179898e1924af671d42a532dc98f862691
Duong Hai <20141340@student.hust.edu.vn>
2021-07-28 02:53:14 +0700

    x

--- nnet/baseline/gnn_fas.py
+++ nnet/baseline/gnn_fas.py
@@ -51,7 +51,7 @@ class GNNFaS(nn.Module):
 
     def __init__(self, args, use_linear=True):
         super().__init__()
-        self.n_filters = [64, 128, 256, 128, 64]
+        self.n_filters = [64, 128, 256, 128, 16]
         self.kernel_size = 3
         self.stride = 2
         self.use_linear = use_linear
