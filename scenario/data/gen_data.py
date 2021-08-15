7ae39e315a831a8772a71b7b689c9f59f685d0bb
Duong Hai <20141340@student.hust.edu.vn>
2021-08-15 12:37:53 +0700

    x

--- scenario/data/gen_data.py
+++ scenario/data/gen_data.py
@@ -8,13 +8,13 @@ from .util import *
 def gen_data(args):
     # step 1: get list of wav for training
     clean_list = get_list('librispeech_360', args)
-    # noise_list = get_list('audioset', args)
+    noise_list = get_list('audioset', args)
 
     # step 2: generate clean (path, start_at) of train, dev, test set
     #         and noise
     config = {
         'clean': split_clean_data(clean_list, args),
-        # 'noise': split_noise_data(noise_list, args),
+        'noise': split_noise_data(noise_list, args),
     }
 
     # step 3a: clear the mixed folder
@@ -35,11 +35,11 @@ def gen_data(args):
         Parallel(n_jobs=os.cpu_count())(
             delayed(f)(i, line) for i, line in enumerate(config['clean'][mode]))
 
-        # print(f'[+] mode={mode}, name=noise')
-        # folder = create_mixed_folder(mode, 'noise')
-        # def f(i, line):
-        #     x = get_firstchannel_read(line)
-        #     path = os.path.join(folder, f'{i}.npz')
-        #     np.savez_compressed(path, x=x)
-        # Parallel(n_jobs=os.cpu_count())(
-        #     delayed(f)(i, line) for i, line in enumerate(config['noise'][mode]))
+        print(f'[+] mode={mode}, name=noise')
+        folder = create_mixed_folder(mode, 'noise')
+        def f(i, line):
+            x = get_firstchannel_read(line)
+            path = os.path.join(folder, f'{i}.npz')
+            np.savez_compressed(path, x=x)
+        Parallel(n_jobs=os.cpu_count())(
+            delayed(f)(i, line) for i, line in enumerate(config['noise'][mode]))
