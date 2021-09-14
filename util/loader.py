7e7897fd9c0f50c13613e396710a03a60b6c2ffc
Hai <duongvanhai187@gmail.com>
2021-09-14 07:48:13 +0700

    x

--- util/loader.py
+++ util/loader.py
@@ -85,6 +85,7 @@ class MixedDataset(Dataset):
         data = np.load(self.path[idx])
         noisy = torch.tensor(data['x'], dtype=torch.float32)
         clean_reverb = torch.tensor(data['y'], dtype=torch.float32)
+        print(data)
         return noisy.squeeze(0), clean_reverb.squeeze(0)
 
 def get_loader(args):
